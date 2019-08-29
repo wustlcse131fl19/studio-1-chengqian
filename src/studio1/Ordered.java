fd04108b1f289505e67f1a35cc6fbd7698a378cd
JieCheng-code <jiecheng@wustl.edu>
2019-08-29 16:58:00 -0500

    Studio1 submission2

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -12,8 +12,10 @@ public class Ordered {
 		int x = ap.nextInt("Value for x?");
 		int y = ap.nextInt("Value for y?");
 		int z = ap.nextInt("Value for z?");
-boolean isOrdered = (x<y);
-
+boolean isOrdered1 = (x<y) && (y<z);
+boolean isOrdered2 = (x>y) && (y>z);
+boolean isOrdered = (isOrdered2) || (isOrdered1);
+System.out.println("The 3 numbers input is ordered. " + isOrdered);
 	}
 
 }
